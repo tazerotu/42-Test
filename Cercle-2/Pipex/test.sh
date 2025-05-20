77dfa51d07b0c6f8675fa10cffcbbca6bbc5e835
tazerotu <162969953+tazerotu@users.noreply.github.com>
2025-05-20 09:56:29 +0200

    Update test.sh

--- Cercle-2/Pipex/test.sh
+++ Cercle-2/Pipex/test.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+rm -rf test.txt
+printf "Ecrivez le contenu de votre fichier d'entree\n"
+printf "Une fois terminer utiliser ctrl + d\n"
+cat > test.txt
 make re
 ./pipex test.txt cat 'wc -l' test2.txt
 printf "\nCommande effectuer : ./pipex test.txt cat 'wc -l' test2.txt\n"
 printf "Equivalent bash : cat test.txt | wc -l > test2.txt\n"
 printf "\nContenu du ficher test.txt :\n"
 cat test.txt
-printf "\n\nResultat du programme :\n"
+printf "\nResultat du programme :\n"
 cat test2.txt
+
+printf "\nReference :\n"
+cat test.txt | wc -l
+
+
+printf "\n\n\n"
+printf "\nCommande effectuer : ./pipex test.txt 'cat -e' 'grep $' test2.txt\n"
+printf "Equivalent bash : cat -e test.txt | grep '$' > test2.txt\n"
+./pipex test.txt "cat -e" "grep '$'" test2.txt
+printf "\nContenu du ficher test.txt :\n"
+cat test.txt
+printf "\nResultat du programme :\n"
+cat test2.txt
+
+printf "\nReference :\n"
+cat -e test.txt | grep "$"
